507ef64c7b30108cb247dab8eea31b7b34851f74
David Würfel <david.wuerfel@googlemail.com>
2020-03-13 18:54:58 +0100

    chore: clean up obsolete files

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,4 +1,4 @@
-it("loads examples", () => {
-  cy.visit("http://localhost:4200");
-  cy.contains("Replace me with something relevant");
+it('loads examples', () => {
+  cy.visit('http://localhost:4200');
+  cy.contains('Replace me with something relevant');
 });
