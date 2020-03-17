986809b03426e0c6757c393dcbab9a239cad9bd4
David Würfel <david.wuerfel@googlemail.com>
2020-03-17 21:16:30 +0100

    feat: basic starting page tests

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,4 +1,18 @@
-it('smoke test', () => {
-  cy.visit('');
-  cy.contains('cypress-e2e-testing-angular app is running!');
+describe('When Angular starting page is loaded', () => {
+  beforeEach(() => {
+    cy.visit('');
+  });
+
+  it('should show the app title', () => {
+    cy.contains('cypress-e2e-testing-angular');
+  });
+
+  it('should show generate component command by default', () => {
+    cy.get('.terminal').contains('ng generate component xyz');
+  });
+
+  it('should show the proper command when Angular Material is selected', () => {
+    cy.contains('Angular Material').click();
+    cy.get('.terminal').contains('ng add @angular/material');
+  });
 });
