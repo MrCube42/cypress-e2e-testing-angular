f732535c92677facd468d987893cf100e3304430
David Würfel <david.wuerfel@googlemail.com>
2020-03-23 07:16:25 +0100

    feat: testing starting page flow

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,4 +1,14 @@
-it('loads examples', () => {
-  cy.visit('/');
-  cy.contains('Replace me with something relevant');
+describe('When Angular starting page is loaded', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('has app title, shows proper command by default and reacts on command changes', () => {
+    cy.contains('cypress-e2e-testing-angular');
+
+    cy.contains('.terminal', 'ng generate component xyz');
+
+    cy.contains('Angular Material').click();
+    cy.contains('.terminal', 'ng add @angular/material');
+  });
 });
